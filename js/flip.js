a4b55b97de7509143158d31413b7a1a94ff22975
4slash271 <4slash271@gmail.com>
2022-03-28 23:57:29 +0900

    fixed header

---
+++ js/flip.js
@@ -0,0 +1,11 @@
+/*************************      글로벌 설정          *************************/
+let width = window.innerWidth;
+let main = document.querySelector('.main')
+let about = document.querySelector('.about')
+let gallery = document.querySelector('.gallery')
+let play = document.querySelector('.play')
+
+/*************************      화면의 너비가 600px보다 작을 때          *************************/
+/*************************      화면의 너비가 1200px보다 작을 때          *************************/
+/*************************      화면의 너비가 1800px보다 작을 때          *************************/
+/*************************      화면의 너비가 2400px보다 작을 때          *************************/
