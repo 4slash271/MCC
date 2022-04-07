a434233224dca971a6b21fd0b2926bfbb7792c44
4slash271 <4slash271@gmail.com>
2022-04-08 01:35:22 +0900

    update tarot card img src

--- js/flip.js
+++ js/flip.js
@@ -9,14 +9,22 @@ let play = document.querySelector('.play')
 let btnNext = document.querySelector('.bt-next');
 let btnPrev = document.querySelector('.bt-prev');
 
-
-
+var index = 1;
 
 function flipNext(){
-    console.log('click');
-
-        
+    if(index<3){
+        index++;
+    }
+    wrapper[index].style.zIndex= 1;
+    wrapper[index-1].style.zIndex= 0;       
 }
-
+function flipPrev(){
+    if(index>1){
+        index=index-1;
+    }
+    wrapper[index].style.zIndex= 1;
+    wrapper[index+1].style.zIndex= 0;       
+}
        
 btnNext.addEventListener('click',flipNext);
+btnPrev.addEventListener('click',flipPrev);
