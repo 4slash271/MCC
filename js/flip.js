917fd4058dfddde3c374e6de9597fa8453f82c21
4slash271 <4slash271@gmail.com>
2022-04-07 23:53:03 +0900

    start flip wrappers

--- js/flip.js
+++ js/flip.js
@@ -1,11 +1,22 @@
 /*************************      글로벌 설정          *************************/
-let width = window.innerWidth;
+
+let wrapper = document.querySelectorAll('.wrapper')
 let main = document.querySelector('.main')
 let about = document.querySelector('.about')
 let gallery = document.querySelector('.gallery')
 let play = document.querySelector('.play')
 
-/*************************      화면의 너비가 600px보다 작을 때          *************************/
-/*************************      화면의 너비가 1200px보다 작을 때          *************************/
-/*************************      화면의 너비가 1800px보다 작을 때          *************************/
-/*************************      화면의 너비가 2400px보다 작을 때          *************************/
+let btnNext = document.querySelector('.bt-next');
+let btnPrev = document.querySelector('.bt-prev');
+
+
+
+
+function flipNext(){
+    console.log('click');
+
+        
+}
+
+       
+btnNext.addEventListener('click',flipNext);
