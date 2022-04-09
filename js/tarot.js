0b6a34d95d5d93e0127fcbe14ec74c130e1ba9ac
4slash271 <4slash271@gmail.com>
2022-04-09 23:57:14 +0900

    change information in tarot slide at about page

--- js/tarot.js
+++ js/tarot.js
@@ -1,5 +1,6 @@
 let tarot = document.querySelector('.content > .tarot__deck ');
 let tarotModal = document.querySelector('.modal--tarot');
+let nextCard = document.querySelector('.modal--tarot .tarot');
 let btnClose = document.querySelector('.modal--tarot .bt-close');
 function openTarot(){
 tarotModal.style.display="flex";
@@ -7,5 +8,9 @@ tarotModal.style.display="flex";
 function closeTarot(){
 tarotModal.style.display="none";
 };
+function slideTarot(){
+    nextCard.style.background="url(../src/img/tarot_back.png) no-repeat center"
+}
 tarot.addEventListener('click', openTarot);
 btnClose.addEventListener('click', closeTarot);
+nextCard.addEventListener('click', slideTarot);
