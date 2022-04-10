9ef735814f36d9705d4b38f320d6fe8b50d28de8
4slash271 <4slash271@gmail.com>
2022-04-10 23:51:17 +0900

    added other card design in tarot.js

--- js/tarot.js
+++ js/tarot.js
@@ -3,13 +3,29 @@ let tarotModal = document.querySelector('.modal--tarot');
 let nextCard = document.querySelector('.modal--tarot .tarot');
 let btnClose = document.querySelector('.modal--tarot .bt-close');
 function openTarot(){
-tarotModal.style.display="flex";
+    tarotModal.style.display="flex";
+    nextCard.style.background="url(../src/img/tarot_back.png) no-repeat center"
+    nextCard.style.backgroundSize="cover"
+   tL();
+    function tL(){
+        nextCard.classList.add("turnL");
+    nextCard.style.background="url(../src/img/magician.png) no-repeat center"
+    nextCard.style.backgroundSize="cover"
+    }
+    
+
 };
+
 function closeTarot(){
 tarotModal.style.display="none";
 };
+
 function slideTarot(){
-    nextCard.style.background="url(../src/img/tarot_back.png) no-repeat center"
+    nextCard.classList.remove("turnL");
+    nextCard.style.background="url(../src/img/magician.png) no-repeat center"
+    nextCard.classList.add("turnR");
+    nextCard.style.background="url(../src/img/queen_sword.png) no-repeat center"
+    nextCard.style.backgroundSize="cover"
 }
 tarot.addEventListener('click', openTarot);
 btnClose.addEventListener('click', closeTarot);
