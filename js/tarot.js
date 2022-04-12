8b46311c6bdda87c3dc15dc95e2e8662ebb6ec9a
4slash271 <4slash271@gmail.com>
2022-04-12 23:35:46 +0900

    edited card slide as divided 3 step

--- js/tarot.js
+++ js/tarot.js
@@ -1,28 +1,35 @@
 let tarot = document.querySelector('.content > .tarot__deck ');
 let tarotModal = document.querySelector('.modal--tarot');
-let nextCard = document.querySelector('.modal--tarot .tarot');
+let firstCard = document.querySelector('.modal--tarot .tarot1');
+let secondCard = document.querySelector('.modal--tarot .tarot2');
+let lastCard = document.querySelector('.modal--tarot .tarot3');
 let btnClose = document.querySelector('.modal--tarot .bt-close');
 
 function openTarot(){
     tarotModal.style.display="flex";
-    nextCard.classList.add("turn-card");
-    nextCard.style.background="url(../src/img/magician.png) no-repeat center"
-    nextCard.style.backgroundSize="cover"
+    firstCard.style.display="flex";   
 };
+function slideFirst(){    
+    secondCard.style.display="flex";
+    firstCard.style.display="none";
+    secondCard.classList.add("turn-card");  
+   }
+function slideSecond(){    
+    secondCard.style.display="none";
+    lastCard.style.display="flex";
+    lastCard.classList.add("turn-card");  
+   }
 
 function closeTarot(){
     tarotModal.style.display="none";
+    lastCard.style.display="none";
 };
-
-function slideTarot(){
-    nextCard.classList.remove("turn-card");
-  
- 
-    // nextCard.classList.add("turn-card");
-    nextCard.style.background="url(../src/img/queen_sword.png) no-repeat center"
-    nextCard.style.backgroundSize="cover"
-}
 
 tarot.addEventListener('click', openTarot);
 btnClose.addEventListener('click', closeTarot);
-nextCard.addEventListener('click', slideTarot);
+firstCard.addEventListener('click', slideFirst);
+secondCard.addEventListener('click', slideSecond);
+
+  
+
+
